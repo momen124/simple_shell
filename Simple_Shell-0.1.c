7ac7d2dfa0cc63744a20294f88060fdf1dcd6404
MuhabGamal <muhabme@gmail.com>
2023-12-03 21:40:43 +0200

    Fixes

--- Simple_Shell-0.1.c
+++ Simple_Shell-0.1.c
@@ -1,57 +1,78 @@
- #include "shell.h"
+#include "shell.h"
 
-   void displayPrompt(void) {
-       printf("$ ");
-   }
+void displayPrompt(void)
+{
+    printf("$ ");
+}
 
-   char *readUserInput(void) {
-       size_t size = 0;
-       char *input = NULL;
-       _getline(NULL, &input, &size);
-       return input;
-   }
+char *readUserInput(void)
+{
+    size_t size = 0;
+    char *input = NULL;
+    _getline(NULL, &input, &size);
+    return input;
+}
 
-   int checkForEOF(char *input) {
-       if (input && input[0] == EOF) {
-           return 1;
-       }
-       return 0;
-   }
+int checkForEOF(char *input)
+{
+    if (input && input[0] == EOF)
+    {
+        return 1;
+    }
+    return 0;
+}
 
-   void executeCommand(char *input) {
-       pid_t pid = fork();
+void executeCommand(char *input)
+{
+    pid_t pid = fork();
 
-       if (pid == 0) {
-           char *argv[] = {"sh", "-c", input, NULL};
-           if (execv("/bin/sh", argv) == -1) {
-               perror("Error");
-           }
-           exit(EXIT_FAILURE);
-       } else if (pid < 0) {
-           perror("Error");
-       } else {
-           int status;
-           waitpid(pid, &status, 0);
-       }
-   }
+    if (pid == 0)
+    {
+        char *argv[4];
+        argv[0] = "sh";
+        argv[1] = "-c";
+        argv[2] = input;
+        argv[3] = NULL;
 
-   void mainLoop(void) {
-       while (1) {
-           displayPrompt();
-           char *input = readUserInput();
+        if (execv("/bin/sh", argv) == -1)
+        {
+            perror("Error");
+        }
+        exit(EXIT_FAILURE);
+    }
+    else if (pid < 0)
+    {
+        perror("Error");
+    }
+    else
+    {
+        int status;
+        waitpid(pid, &status, 0);
+    }
+}
 
-           if (checkForEOF(input)) {
-               free(input);
-               break;
-           }
+void mainLoop(void)
+{
+    while (1)
+    {
+        char *input; // Declare at the beginning
 
-           executeCommand(input);
-           free(input);
-       }
-   }
+        displayPrompt();
+        input = readUserInput(); // Assign later
 
-   int main() {
-       mainLoop();
-       return 0;
-   }
-   
+        if (checkForEOF(input))
+        {
+            free(input);
+            break;
+        }
+
+        executeCommand(input);
+        free(input);
+    }
+}
+
+int main()
+{
+    mainLoop();
+    return 0;
+}
