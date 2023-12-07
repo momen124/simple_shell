7b289211795ab9d11f6231bd16cabbe3bd98c047
momen124 <momenhossny2004@gmail.com>
2023-12-07 03:48:20 +0200

    linking1

--- cmd_exec.c
+++ cmd_exec.c
@@ -10,25 +10,22 @@
 char *find_path(info_t *info, char *path, char *command)
 {
     (void)info;
-    
-    char *token; // Move the declaration to the beginning
+
+    char *token;
     char *full_path;
     struct stat st;
 
     token = strtok(path, ":");
-    while (token != NULL)
-    {
+    while (token != NULL) {
         full_path = malloc(strlen(token) + strlen(command) + 2);
-        if (!full_path)
-        {
-            return NULL; 
+        if (!full_path) {
+            return NULL;
         }
-        
+
         sprintf(full_path, "%s/%s", token, command);
 
-        if (stat(full_path, &st) == 0)
-        {
-            return full_path; 
+        if (stat(full_path, &st) == 0) {
+            return full_path;
         }
 
         free(full_path);
