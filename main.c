f029635feccbee6635953196cce9cfc96ce8b953
Muhab Gamal <muhabme@gmail.com>
2023-12-12 00:00:05 +0200

    Handling && and ||

--- main.c
+++ main.c
@@ -4,51 +4,54 @@
  * main - Entry point for the shell program.
  * Return: Exit status.
  */
-int main(int argc, char *argv[])
-{
-info_t info = {0}; 
+int main(int argc, char *argv[]) {
+info_t info = {0};
 char *user_input;
 int builtin_ret;
+int last_command_status = 0;
+int i;
+operator_command_struct *commands;
 
-if (argc > 1) 
-{
+if (argc > 1) {
 freopen(argv[1], "r", stdin);
 }
 
-while (1) 
-{
+while (1) {
+if (isatty(STDIN_FILENO)) {
 display_prompt();
+}
+
 user_input = read_user_input();
-
-if (user_input == NULL) 
-{
-break; 
-/* End of file or an error */
+if (user_input == NULL || user_input[0] == '\0') {
+free(user_input);
+break;
 }
 
-tokenize_command(&info, user_input);
-if (info.token_count > 0)
-{
+commands = parse_operators(user_input);
+
+for (i = 0; i < commands->count; i++) {
+if ((i > 0 && strcmp(commands->operators[i - 1], "&&") == 0 && last_command_status != 0) ||
+(i > 0 && strcmp(commands->operators[i - 1], "||") == 0 && last_command_status == 0)) {
+continue;
+}
+
+tokenize_command(&info, commands->command_list[i]);
+if (info.token_count > 0) {
 builtin_ret = find_builtin(&info);
-if (builtin_ret == 0)
-{
-continue; 
-/* Built-in command executed, continue loop */
-}
-else if (builtin_ret < 0)
-{
-find_and_execute_command(&info); 
-/* Find and execute non-built-in command */
+if (builtin_ret != 0) {
+find_and_execute_command(&info);
 }
 }
 
-free_info(&info, 0); 
-/* Reset info for the next iteration */
+last_command_status = info.status;
+free_info(&info, 0);
+}
+
+free_operator_commands(commands);
 free(user_input);
 }
 
-free_info(&info, 1); 
-/* Clean up before exiting */
+free_info(&info, 1);
 return info.status;
 }
 
@@ -68,19 +71,20 @@ char *read_user_input(void)
 {
 char *input = NULL;
 size_t size = 0;
-ssize_t read_bytes = getline(&input, &size, stdin);
-
+ssize_t read_bytes;
+if (feof(stdin))
+{
+return NULL;
+}
+read_bytes = getline(&input, &size, stdin);
 if (read_bytes == -1)
 {
 free(input);
 return NULL;
 }
-
-/* Remove newline character if present */
 if (input[read_bytes - 1] == '\n')
 {
 input[read_bytes - 1] = '\0';
 }
-
 return input;
 }
